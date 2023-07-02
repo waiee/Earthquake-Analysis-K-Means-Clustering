7c70608609c4cf153fd822a33f17998b25166164
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-07-02 22:24:31 +0800

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 import pydeck as pdk
