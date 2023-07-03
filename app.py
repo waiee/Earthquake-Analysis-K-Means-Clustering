a90ed6701759f7674132c15d4dce82901da06ab8
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-07-03 20:27:50 +0800

    test

--- app.py
+++ app.py
@@ -368,5 +368,12 @@ elif selected == "Model Comparison":
     method()
 
 elif selected == "About":
-    st.write("Hi, I am Waiee.")
+    dp_image = Image.open("image/output4.png")
+    image_column, right_column = st.columns((1,2))
+    with image_column:
+        st.image(dp_image, caption="")
+    with right_column:
+        st.subheader("Hi, I am Waiee :wave:")
+        st.title("Bachelor of Computer Science in Data Science")
+        st.write("I am passionate in Data Science, Data Analysis, AI and Machine Learning.")
 
