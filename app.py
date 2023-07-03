378986a14f199e713dc4689bca56a75607613cd7
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-07-03 20:38:13 +0800

    fx

--- app.py
+++ app.py
@@ -343,7 +343,16 @@ def method():
         Further studies are needed in future in order to obtain better and more accurate results.
         """
     )
-    
+
+def about():
+    dp_image = Image.open("image/removebgWaiee.png")
+    image_column, right_column = st.columns((1,2))
+    with image_column:
+        st.image(dp_image, caption="")
+    with right_column:
+        st.subheader("Hi, I am Waiee :wave:")
+        st.title("Bachelor of Computer Science (Hons.) Data Science")
+        st.write("I am passionate in Data Science, AI and Machine Learning.")
 
 
 
@@ -368,12 +377,5 @@ elif selected == "Model Comparison":
     method()
 
 elif selected == "About":
-    dp_image = Image.open("image/removebgWaiee.png")
-    image_column, right_column = st.columns((1,2))
-    with image_column:
-        st.image(dp_image, caption="")
-    with right_column:
-        st.subheader("Hi, I am Waiee :wave:")
-        st.title("Bachelor of Computer Science (Hons.) Data Science")
-        st.write("I am passionate in Data Science, AI and Machine Learning.")
+    about()
 
